d6d10a76f103be4a217f8ef722d6ef5c76776f4a
icelor <icelor01@ucm.es>
2020-08-03 13:23:19 +0200

    Versión inicial juego: comprueba xmin y xmax, y muestra si es correcto.

--- Assets/InputWindow/Scripts/UI_InputOK.cs
+++ Assets/InputWindow/Scripts/UI_InputOK.cs
@@ -4,31 +4,111 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using CodeMonkey.Utils;
+//using TMPro;
 
 public class UI_InputOK : MonoBehaviour {
 
-    public Button OKButton;
-    public Button okBtn1;
-    public Button okBtn2;
-    //private Button_UI cancelBtn;
+    private static UI_InputOK instance;
+    public Button_UI okBtn;
+     //private Button_UI cancelBtn;
     [SerializeField] public GameObject plotComponent;
-    public GameObject feedbackOK;
-    public GameObject feedbackNOK;
     private int xMin;
     private int xMax;
     private int n;
     private int xMin_solution;
     private int xMax_solution;
-    
+    public GameObject feedbackOK;
+    public GameObject feedbackNOK;
+
     // Use this for initialization
-    void Update () {
-        OKButton.onClick.AddListener(TaskOnClick1);
-        //okBtn1.onClick.AddListener(TaskOnClick2);
-        //okBtn2.onClick.AddListener(TaskOnClick3);
+    private void Awake()
+    {
+        instance = this;
+        Hide();
+        okBtn = transform.Find("okBtn").GetComponent<Button_UI>();
+      }
+
+    // Use this for initialization
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            okBtn.ClickFunc();
+        }
+        /* if (Input.GetKeyDown(KeyCode.Escape))
+         {
+            cancelBtn.ClickFunc();
+        }*/
 
     }
 
-      void TaskOnClick1()  {
+
+    private void Show(string inputString, Action onCancel, Action<string> onOk)
+    {
+
+        Plot plot = plotComponent.GetComponent<Plot>();
+        Table table = plot.getTable();
+        xMin = table.Getxmin();
+        xMax = table.Getxmax();
+        n = table.Getn();
+        xMin_solution = table.Getxmin_solution();
+        xMax_solution = table.Getxmax_solution();
+
+        if (xMin_solution == xMin & xMax_solution == xMax)
+        {
+            Debug.Log("Respuesta correcta");
+
+            //feedback = GameObject.Find("FeedbackOK");
+            feedbackOK.SetActive(true);
+
+        }
+        else
+        {
+            //feedback = GameObject.Find("FeedbackNOK");
+            feedbackNOK.SetActive(true);
+            Debug.Log("Respuesta incorrecta");
+            //respuestaIncorrecta.SetActive(true);
+        }
+
+
+
+
+        gameObject.SetActive(true);
+
+
+    
+
+        okBtn.ClickFunc = () => {
+                        Hide();
+        };
+
+        /*cancelBtn.ClickFunc = () => {
+            Hide();
+            onCancel();
+        };*/
+
+    }
+
+    public void Hide()
+    {
+        feedbackOK.SetActive(false);
+        feedbackNOK.SetActive(false);
+        gameObject.SetActive(false);
+       
+    }
+
+ 
+    public static void Show_Static(string inputString, Action onCancel, Action<string> onOk)
+    {
+        instance.Show(inputString, onCancel, onOk);
+
+     }
+
+
+
+    /*
+    void TaskOnClick1()  {
         //Output this to console when ButtonOK is clicked
         //Comprobamos si los valores de xmin y max introducidos coinciden con los de la solucion esperada
 
@@ -66,7 +146,7 @@ public class UI_InputOK : MonoBehaviour {
         feedbackNOK.SetActive(false);
 
     }
-
+    */
 
 
 
---
+++ Assets/InputWindow/Scripts/UI_OKTesting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+public class UI_OKTesting : MonoBehaviour {
+
+
+    private void Start()
+    {
+       
+        transform.Find("OKButton").GetComponent<Button_UI>().ClickFunc = () => {
+            //inputSpan.Show(() => {
+            UI_InputOK.Show_Static("", () => {
+                // Clicked Cancel
+                CMDebug.TextPopupMouse("Cancel!");
+            }, (string inputText) => {
+                // Clicked Ok
+                CMDebug.TextPopupMouse("Ok: " + inputText);
+
+            });
+        };
+
+    }
+}
