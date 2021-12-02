1aaefb6be9f8bb1a4f194e527dc1fcb26574fafb
icelor <icelor01@ucm.es>
2021-12-02 10:55:37 +0100

    Incluimos scripts JSON para poder cargar ejercicios a partir de un archivo

---
+++ Assets/Graph/Scripts/Exercise.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+
+[System.Serializable]
+public class Exercise
+{
+    public int id;
+    public string type;
+    public string url;
+    public string musica;
+    public string fileName;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    public void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public static Exercise Load(String fileName)
+    {
+
+        // Deserialización
+        StreamReader reader = new StreamReader(Application.dataPath + "/" + fileName);
+        string jsonString = reader.ReadToEnd();
+        Debug.Log("JSON loaded string is: " + jsonString);
+        Exercise myExercise = JsonUtility.FromJson<Exercise>(jsonString);
+        return myExercise;
+        //Debug.Log("JSON id is: " + myExercise.id);
+    }
+
+    private void JsonTextReader(object sr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save()
+    {
+        // Serialización
+
+        /*
+        Exercise myExercise = new Exercise();
+        myExercise.id = 1;
+        myExercise.url = "http://localhost:5000/f/new_process/iname_theta?xmin=0&xmax=10&numValues=30&numGraphs=2&a=1&b=2";
+        myExercise.type = "media_y_correlacion";
+        myExercise.musica = "titulo_cancion.mp3";
+        */
+
+        //string json = JsonUtility.ToJson(myExercise);
+        string json = JsonUtility.ToJson(this);
+        Debug.Log("JSON saved string is: " + json);
+
+    }
+
+
+}
---
+++ Assets/Graph/Scripts/ExerciseList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+[System.Serializable]
+public class ExerciseList
+{
+    public List<Exercise> Exercises;
+    // Particularizar para tutoriales o niveles
+    public string type;
+
+    public static ExerciseList FromJson(string listJsonFile)
+    {
+        // Deserialización
+        StreamReader reader = new StreamReader(Application.dataPath + "/" + listJsonFile);
+        string jsonListString = reader.ReadToEnd();
+        Debug.Log("JSON exercises string is: " + jsonListString);
+        Debug.Log("JSON exercise List loaded is: " + JsonUtility.FromJson<ExerciseList>(jsonListString));
+        ExerciseList exerciseList = JsonUtility.FromJson<ExerciseList>(jsonListString);
+        Debug.Log("JSON exercise List loaded is: " + exerciseList.ToJson());
+        return exerciseList;
+    }
+
+
+    public string ToJson()
+    {
+        return JsonUtility.ToJson(this);
+
+    }
+
+}
